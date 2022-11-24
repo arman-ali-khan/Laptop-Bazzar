95c7e4be7a7fa294d61b833af7c7bd7894e6c040
Arman Khan <sparmankhan@gmail.com>
2022-11-24 12:18:09 +0600

    login page added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Blog from "../components/Pages/Blog/Blog";
 import Category from "../components/Pages/Category/Category";
 import ErrorPage from "../components/Pages/ErrorPage/ErrorPage";
 import Home from "../components/Pages/Home/Home";
+import Login from "../components/Pages/Login/Login";
 import Wishlist from "../components/Pages/Wishlist/Wishlist";
 import Main from "../Layout/Main";
 
@@ -11,7 +12,7 @@ export const router = createBrowserRouter([
         {path:'/', element:<Home/>},
         {path:'/wishlist', element:<Wishlist/>},
         {path:'/blog', element:<Blog/>},
-        // {path:'/login', element:<Login/>},
+        {path:'/login', element:<Login/>},
         // {path:'/register', element:<Register/>},
         {path:'/category/:id', element:<Category/>},
     ]}
